83a10e47c22ed1ad8615b98464255f2109b6226c
gugasanchez <11802440@usp.br>
2020-06-23 14:31:24 +0000

    entrega m008

--- m008.c
+++ m008.c
@@ -22,20 +22,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 256
 
 /* Converts the string 's' from first-middle-last name 
    form into LAST-first-middle form. */
-
-void lastname (char *s)
+void lastname (char *c)
 {
+  char copied[MAX];
+  char changed[MAX];
+  int i, j, length, space;
+
+  strcpy(copied, c);
+  length = strlen(copied);
+
+  for(i=0; copied[i]!=0; i++) {
+    if(copied[i] == ' ') {
+      space = i;
+    }
+  }
+
+  for(j=(space+1); j<length; j++) {
+    if((copied[j] >= 'a') && (copied[j] <= 'z')) {
+      changed[j-(space+1)] = copied[j]-32;
+    }
+    else {
+      changed[j-(space+1)] = copied[j];
+    }
+  }
+
+  changed[length-(space+1)] = ',';
+  changed[length-space] = ' ';
+
+  for(j=0; j<space; j++) {
+    changed[length-space+1+j] = copied[j];
+    changed[length+1] = 0;
+  }
+
+  strcpy(c, changed);
 }
 
-
-
 /* Do not edit this function. */
-
 int main (int argc, char **argv)
 {
   int n;
